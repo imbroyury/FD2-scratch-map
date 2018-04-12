ca793467dd149c1d623f5de0bbe561ce9943d756
Yury <imbroyury@gmail.com>
2018-04-12 12:05:56 +0300

    Update server.js

--- server.js
+++ server.js
@@ -35,9 +35,7 @@ app.use(session({
 }));
 
 // Start Server
-app.listen('3000', () => {
-    console.log('Server started on port 3000...');
-});
+app.listen('3000', () => console.log('Server started on port 3000...'));
 
 // Passport Config
 require('./config/passport')(passport);
